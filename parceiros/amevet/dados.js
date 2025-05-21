882162384948af23ecd9958a8b3b66fdf0e965e1
SmartPainelTam-PE <smartpaineltamandare@gmail.com>
2025-05-21 17:28:34 -0300

    Add files via upload

---
+++ parceiros/amevet/dados.js
@@ -0,0 +1,30 @@
+// Dados do perfil
+const dadosPerfil = {
+    nome: "AmeVet",
+    descricao: "Centro Médico Veterinário",
+    cardapio: "",
+    localizacao: "https://www.google.com.br/maps/place/AmeVet+-+Centro+M%C3%A9dico+Veterin%C3%A1rio/@-8.7510445,-35.0999696,17z/data=!3m1!4b1!4m6!3m5!1s0x70079c5c0f04821:0x59b57a4cb38a6b69!8m2!3d-8.7510498!4d-35.0973947!16s%2Fg%2F11tms10496?hl=pt-BR&entry=ttu&g_ep=EgoyMDI1MDUxNS4wIKXMDSoASAFQAw%3D%3D",
+    whatsapp: "5581981190391",
+    instagram: "@amevetpe",
+    facebook: "/share/1AQdyMRjv3/",
+    website: "",
+    ifood: "",
+    anotaai: "",
+    contatos: "81981190391",
+    tema: "turquoise"
+  };
+  
+  // Ordem das chaves para manter a mesma sequência do arquivo original
+  const ordemChaves = [
+    "nome", 
+    "descricao", 
+    "cardapio",
+    "localizacao", 
+    "whatsapp", 
+    "instagram", 
+    "facebook", 
+    "website", 
+    "ifood", 
+    "anotaai", 
+    "contatos"
+  ]; 
