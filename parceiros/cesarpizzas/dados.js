44aa0e1b545aba585ac736d754fd849a6ecbbc29
SmartPainelTam-PE <smartpaineltamandare@gmail.com>
2025-05-21 21:34:47 -0300

    Add files via upload

---
+++ parceiros/cesarpizzas/dados.js
@@ -0,0 +1,30 @@
+// Dados do perfil
+const dadosPerfil = {
+  nome: "Cesar Pizzas",
+  descricao: "Restaurante e Pizzaria",
+  cardapio: "Cardápio",
+  localizacao: "https://www.google.com/maps/place/C%C3%A9sar+Pizzas/@-8.7528973,-35.1015215,17z/data=!3m1!4b1!4m6!3m5!1s0x70079ca2fa730ad:0x9691212945a4e2cc!8m2!3d-8.7529026!4d-35.0989466!16s%2Fg%2F11n0576p_w?entry=ttu&g_ep=EgoyMDI1MDUxNS4xIKXMDSoASAFQAw%3D%3D",
+  whatsapp: "5581995142166",
+  instagram: "@cesarpizzas_",
+  facebook: "",
+  website: "",
+  ifood: "https://www.ifood.com.br/delivery/tamandare-pe/cesar-pizzasrestaurante-centro/e244d110-7f6f-4f0b-a60e-5552a465fd47",
+  anotaai: "",
+  contatos: "81995142166",
+  tema: "dark"
+};
+
+// Ordem das chaves para manter a mesma sequência do arquivo original
+const ordemChaves = [
+  "nome", 
+  "descricao", 
+  "cardapio",
+  "localizacao", 
+  "whatsapp", 
+  "instagram", 
+  "facebook", 
+  "website", 
+  "ifood", 
+  "anotaai", 
+  "contatos"
+]; 
