bef3aab36f4def822ec5e1a72538e956bd158d8a
SmartPainelTam-PE <smartpaineltamandare@gmail.com>
2025-05-21 23:31:06 -0300

    Add files via upload

---
+++ parceiros/borabora/dados.js
@@ -0,0 +1,30 @@
+// Dados do perfil
+const dadosPerfil = {
+    nome: "Bora Bora",
+    descricao: "Restaurante, bar e estrutura pé na areia à beira-mar. Reserve seu dia com conforto, boa comida e uma das vistas mais paradisíacas do Brasil.",
+    cardapio: "",
+    localizacao: "https://www.google.com/maps/place/Restaurante+Bora+Bora+Carneiros/@-8.7032192,-35.0846212,17z/data=!3m1!4b1!4m6!3m5!1s0x7aa82f4e12283ab:0x8ab34b1246747010!8m2!3d-8.7032245!4d-35.0797503!16s%2Fg%2F1q6j2y0cm?entry=ttu&g_ep=EgoyMDI1MDUxNS4xIKXMDSoASAFQAw%3D%3D",
+    whatsapp: "5581991640673",
+    instagram: "@boraboracarneiros",
+    facebook: "/boraboracarneiros",
+    website: "www.bbcarneiros.com.br",
+    ifood: "",
+    anotaai: "",
+    contatos: "81991640673",
+    tema: ""
+  };
+  
+  // Ordem das chaves para manter a mesma sequência do arquivo original
+  const ordemChaves = [
+    "nome", 
+    "descricao", 
+    "cardapio",
+    "localizacao", 
+    "whatsapp", 
+    "instagram", 
+    "facebook", 
+    "website", 
+    "ifood", 
+    "anotaai", 
+    "contatos"
+  ]; 
