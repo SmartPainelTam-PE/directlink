1faf8275104d33190fdef03a6b7c79e9ac2f98c0
SmartPainelTam-PE <smartpaineltamandare@gmail.com>
2025-05-22 03:40:57 -0300

    Add files via upload

---
+++ parceiros/submarino/dados.js
@@ -0,0 +1,28 @@
+const dadosPerfil = {
+    nome: "Submarino Amarelo",
+    descricao: "Bar e restaurante temático dos Beatles",
+    localizacao: "https://www.google.com/maps/place/Submarino+Amarelo/@-8.7532936,-35.097867,17z/data=!3m1!5s0x70078fb3a2b9205:0x99cb31ece24bceae!4m6!3m5!1s0x700798b02af836b:0x64e396dfd0d3cbe3!8m2!3d-8.7532989!4d-35.0929961!16s%2Fg%2F11gxrz3pd5?entry=ttu&g_ep=EgoyMDI1MDUxNS4xIKXMDSoASAFQAw%3D%3D",
+    instagram: "@submarino.a",
+    whatsapp: "",
+    facebook: "/barsubamarelo",
+    website: "",
+    ifood: "",
+    anotaai: "",
+    contatos: "8136761361",
+    tema: "yellow",
+    
+    }
+    const ordemChaves = [
+    "nome", 
+    "descricao", 
+    "cardapio",
+    "localizacao", 
+    "whatsapp", 
+    "instagram", 
+    "facebook", 
+    "website", 
+    "ifood", 
+    "anotaai", 
+    "contatos"
+  ]; 
+
