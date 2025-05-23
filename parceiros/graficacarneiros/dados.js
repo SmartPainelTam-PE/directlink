18315b815ca12bcc4668a88044a53df26e7ff012
SmartPainelTam-PE <smartpaineltamandare@gmail.com>
2025-05-23 13:03:57 -0300

    Update dados.js

--- parceiros/graficacarneiros/dados.js
+++ parceiros/graficacarneiros/dados.js
@@ -2,13 +2,13 @@
 const dadosPerfil = {
     nome: "Gráfica Carneiros",
     descricao: "Decoração Ambientes, Banner’s, Backdrops, Adesivos, Produtos Personalizados Festas e Eventos, e Muito Mais!",
-    cardapio: "Cardápio",
+    cardapio: "",
     localizacao: "https://www.google.com/maps/place/Carneiros+Comunica%C3%A7%C3%A3o+Visual+%26+Gr%C3%A1fica/@-8.7502258,-35.0992436,17z/data=!3m1!4b1!4m6!3m5!1s0x70078e59d41e811:0x929c12fe30e21d4d!8m2!3d-8.7502311!4d-35.0966687!16s%2Fg%2F11sk88rkxc?entry=ttu&g_ep=EgoyMDI1MDUxNS4xIKXMDSoASAFQAw%3D%3D",
     whatsapp: "5581988297601",
     instagram: "@carneiroscv",
     facebook: "/graficacarneiros",
     website: "graficacarneiros.com.br",
-    ifood: "www.ifood.com",
+    ifood: "",
     anotaai: "",
     contatos: "81988297601",
     tema: "pink"
